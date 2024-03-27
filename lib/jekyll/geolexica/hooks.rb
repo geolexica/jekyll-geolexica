8e36f5564062456ff66cd560521beb0035c4aae5
HassanAkbar <hassanakbar2@gmail.com>
2024-03-27 09:13:30 +0500

    Adding support for stem equations (#24)
    * Adding support for stem equations
    
    * added test cases

--- lib/jekyll/geolexica/hooks.rb
+++ lib/jekyll/geolexica/hooks.rb
@@ -11,6 +11,7 @@ module Jekyll
         hook :post_read, :site, :load_glossary
         hook :pre_render, :documents, :expose_glossary
         hook :pre_render, :pages, :expose_glossary
+        hook :post_render, :pages, :convert_math
       end
 
       # Adds Jekyll::Site#glossary method, and initializes an empty glossary.
@@ -26,6 +27,26 @@ module Jekyll
 
       def expose_glossary(page_or_document, liquid_drop)
         liquid_drop["glossary"] = page_or_document.site.glossary
+      end
+
+      def convert_math(page)
+        page.output.gsub!(/stem:\[([^\]]*?)\]/) do
+          ascii_equation = CGI.unescapeHTML(Regexp.last_match[1])
+
+          mathml_equation = ::Plurimath::Math
+                              .parse(ascii_equation, :asciimath)
+                              .to_mathml
+
+          # temporary hack to use display inline for math equations because
+          # currently there is no option to use display inline in plurimath
+          mathml_equation.gsub!("display=\"block\"", "display=\"inline\"")
+
+          # Removing newlines(\n) and escaping double quotes(")
+          # because they will cause parsing issues in json
+          mathml_equation.gsub!("\n", "").gsub!("\"", "\\\"") unless page.html?
+
+          mathml_equation
+        end
       end
 
       def hook event, target, action
